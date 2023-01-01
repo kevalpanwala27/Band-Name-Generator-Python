30cc8a85e8caaee65f185e1e8d30c06423e8a577
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2023-01-01 23:43:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to band name generator")
+city_name = input("Whats name of your city\n")
+print(city_name)
+pet_name = input("What is your pet name\n")
+print(pet_name)
+print("Your band name is: " + city_name + " " + pet_name)
+
